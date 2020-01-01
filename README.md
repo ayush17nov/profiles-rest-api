2e2a7bfd771bd248de2e8eaf27aad5a08ee96570
amit <kamit6@cisco.com>
2020-01-01 16:16:33 +0530

    created models for jango

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ profiles info using django rest framework
 
 # create virtual env
 python -m venv ~/restenv
+
+# link to pep guidlines for code standards
+https://www.python.org/dev/peps/pep-0008/
--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    """Manager for User Profiles"""
+
+    def create_user(self, email, name, password=None):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError("It is must to provided email to create a user")
+
+        email = self.normalize_email(email)
+        user = self.model(email= email, name=name)
+        
+        user.set_password(password)
+        user.save(using = self._db)
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Create and save a super user with provided details"""
+        user = self.create_user(email, name, password)
+        user.is_superuser = True
+        user.is_staff = True
+
+        user.save(using = self._db)
+
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database models for users in system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """Retriev Full Name of the user"""
+        return self.name
+
+    def get_short_name(self):
+        """Retriev Short Name of the user"""
+        return self.name
+
+    def __str__(self):
+        """String representation of a user"""
+        return self.email
